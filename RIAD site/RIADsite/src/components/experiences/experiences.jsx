543c98948f1fb81d5ba74b602a51479897a740e7
ouamine59 <mohamed.manseur@gmail.com>
2024-02-01 10:40:26 +0100

    fix article about exeprience

--- RIAD site/RIADsite/src/components/experiences/experiences.jsx
+++ RIAD site/RIADsite/src/components/experiences/experiences.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './experiences.css'
 const Experiences = () => {
   return (
-    <div className='div'>
+    <article className='div'>
         <h1>Notre expériences</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium molestiae autem unde at, pariatur cumque.</p>
-    </div>
+    </article>
   )
 }
 
